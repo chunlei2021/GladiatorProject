3bc943452d41313935318b9a375cdf5693723846
chunlei <87575039+chunlei2021@users.noreply.github.com>
2024-12-14 18:26:19 +0100

    Inizio del progetto di fine anno
    La creazione del file main

---
+++ Main.java
@@ -0,0 +1,12 @@
+/*Progetto di fine anno sui Gladiatori
+[Realizzato da Camozzo, Ferrari e Petrini] 
+*/
+
+import java.util.Scanner;
+import java.util.Random;
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Benvenuto nel gioco dei Gladiatori!");
+        
+    }
+}

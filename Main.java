aa39b1b7f9336cf7a1bb5f251104d8620230af78
chunlei <87575039+chunlei2021@users.noreply.github.com>
2025-01-04 09:55:55 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -43,7 +43,7 @@ public class Main{
         
         if(inizio.equals("si")){
             
-            System.out.println(blue + "\n--- INIZIA LA BATTAGLIA! ---\n" + reset);
+            System.out.println(blue + "\n---- INIZIA LA BATTAGLIA! ----\n" + reset);
             //Inizio vero proprio della battaglia
             String risposta = "si";
             boolean risp = true;
@@ -52,9 +52,11 @@ public class Main{
 
                 if(g[0].getPuntiSalute() <= 0){
                     System.out.println(red + bold + "Il tuo Gladiatore \"" + g[0].getNome() + "\" ha perso!" + reset); // Stampare gli HP rimasti, e stampare che l'altro gadiatore ha vinto
+                    g[0].checkStatus(g[0]);
                     break;
                 } else if(g[1].getPuntiSalute() <= 0){
                     System.out.println(green + bold + "Il Gladiatore \"" + g[1].getNome() + "\" è morto! Hai vinto!!" + reset); // Stampare gli HP rimasti, e stampare che l'altro gladiatore ha vinto
+                    g[1].checkStatus(g[1]);
                     break;
                 }
                 System.out.println("[Per vedere la lista dei gladiatori scrivere \"listagladiatori\"]");
@@ -83,8 +85,10 @@ public class Main{
             g[1].incrementaLivello(g[1].getEsperienza());
             System.out.println("Il Gladiatore \"" + g[1].getNome() + "\" ha guadagnato " + g[1].getEsperienza() + " XP! [Livello: " + g[1].getLivello() + "]");
             System.out.println("Il tuo Gladiatore \"" + g[0].getNome() + "\" ha guadagnato " + g[0].getEsperienza() + " XP! [Livello: " + g[0].getLivello() + "]");
-            //Continuare a scrivere il codice di questo blocco graffo per la battaglia
-            System.out.println(blue + "\n--- FINE BATTAGLIA! ---" + reset);
+            //Fine battaglia
+            System.out.println("\nLa lista dei gladiatori disponibili: ");
+            g[0].listaGladiatori(g);
+            System.out.println(blue + "\n---- FINE BATTAGLIA! ----" + reset);
     } else {
         System.out.println(red + "Fine del gioco!" + reset);
     }
